9115d70adb035b01d12f673f4e65b8c6e51289bc
ToniSardinha64 <45799376+ToniSardinha64@users.noreply.github.com>
2020-05-20 00:54:34 +0100

    Add files via upload

--- lighting2/shaders/2.1.basic_lighting_walls.fs
+++ lighting2/shaders/2.1.basic_lighting_walls.fs
@@ -1,7 +1,7 @@
 #version 330 core
 out vec4 FragColor;
 
-struct Plan
+struct ExtWall
 {
     vec3 ambient;
     vec3 diffuse;
@@ -22,27 +22,27 @@ in vec3 FragPos;
 
   
 uniform vec3 viewPos;
-uniform Plan plan;
+uniform ExtWall extWall;
 uniform Luz light;
 
 void main()
 {
-    // ambient Plan
-    vec3 ambientPlan = light.ambient * plan.ambient;
+    // ambient ExtWall
+    vec3 ambientExtWall = light.ambient * extWall.ambient;
     
-    // diffuse Plan
-    vec3 normPlan = normalize(Normal);
-    vec3 lightDirPlan = normalize(light.pos - FragPos);
-    float diffPlan = max(dot(normPlan, lightDirPlan), 0.0);
-    vec3 diffusePlan = light.diffuse * (diffPlan * plan.diffuse);
+    // diffuse ExtWall
+    vec3 normExtWall = normalize(Normal);
+    vec3 lightDirExtWall = normalize(light.pos - FragPos);
+    float diffExtWall = max(dot(normExtWall, lightDirExtWall), 0.0);
+    vec3 diffuseExtWall = light.diffuse * (diffExtWall * extWall.diffuse);
 
-    // specular Plan
-    vec3 viewDirPlan = normalize(viewPos - FragPos);
-    vec3 reflectDirPlan = reflect(-lightDirPlan, normPlan);
-    float specPlan = pow(max(dot(viewDirPlan, reflectDirPlan), 0.0), plan.shine);
-    vec3 specularPlan = light.specular * (specPlan * plan.specular);
+    // specular ExtWall
+    vec3 viewDirExtWall = normalize(viewPos - FragPos);
+    vec3 reflectDirExtWall = reflect(-lightDirExtWall, normExtWall);
+    float specExtWall = pow(max(dot(viewDirExtWall, reflectDirExtWall), 0.0), extWall.shine);
+    vec3 specularExtWall = light.specular * (specExtWall * extWall.specular);
             
-    vec3 resultPlan = ambientPlan + diffusePlan + specularPlan;
-    FragColor = vec4(resultPlan, 1.0);
+    vec3 resultExtWall = ambientExtWall + diffuseExtWall + specularExtWall;
+    FragColor = vec4(resultExtWall, 1.0);
 
 }   
