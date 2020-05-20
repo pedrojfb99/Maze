9db66b1f7e1979bf4c22cdd880fadaf2e1f829cd
rui-raposo <ruiraposo7@gmail.com>
2020-05-20 23:29:13 +0100

    jogabilidade done

--- lighting2/camera.h
+++ lighting2/camera.h
@@ -4,6 +4,7 @@
 #include <GL/glew.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <math.h>
 
 #include <vector>
 
@@ -18,8 +19,8 @@ enum Camera_Movement {
 // Default camera values
 const float YAW         = -90.0f;
 const float PITCH       =  0.0f;
-const float SPEED       =  10.0f;
-const float SENSITIVITY =  0.01f;
+const float SPEED       =  5.0f;
+const float SENSITIVITY =  0.2f;
 const float ZOOM        =  45.0f;
 
 
@@ -67,303 +68,214 @@ public:
         return glm::lookAt(Position, Position + Front, Up);
     }
 
-    bool EvaluateCoordinates(float x, float y){
+    int EvaluateCoordinates(float x, float y){
 
         printf("(%f, %f)\n", x, y);
 
         // Paredes Exteriores
         if(x < -19.8 ){
-            return false;
+            return -1;
         }
         if(x > 19.8 ){
-            return false;
+            return -1;
         }
         if(y < -19.5) {
-            return false;
+            return -1;
         }
         if(y > 19.5){
-            return false;
+            return -1;
         }
-
-
-
-
-
 
         // Blocos
         if( ((x < -14.75) && y > 11.75)){
-            return false;
+            return -1;
         }
 
         if( (x > -12.2 && x  < -6.8 ) && (y > 11.75)){
-            return false;
+            return -1;
         }
 
-
-
         if (( x > -4.2 && x < 13.2) && ( y < 18.2 && y > 11.8)){
-            return false;
+            return -1;
         }
 
 
         if (( x > 2.8 && x < 7.2) && ( y < 9.2 && y > 4.8)){
-            return false;
+            return -1;
         }   
 
-
-
-       if (( x > 8.8 && x < 13.2) && ( y < 9.2  && y > 4.8)){
-            return false;
+        if (( x > 8.8 && x < 13.2) && ( y < 9.2  && y > 4.8)){
+            return -1;
         }
 
 
         if (( x > -0.2 && x < 13.2) && ( y < -10.8 && y > -18.2)){
-            return false;
+            return -1;
         }
 
 
-
         if (( x > 7.8 && x < 13.2) && ( y < -2.8 && y > -18.2 )){
-            return false;
+            return -1;
         }
 
 
         if (( x > -4.2 && x < 4.2) && ( y < -2.8 && y > -7.2 )){
-            return false;
+            return -1;
         }
 
 
         if (( x > -9.2 && x < -3.8) && ( y < -15.8 && y > -18.2 )){
-            return false;
+            return -1;
         }
 
 
         if (( x > -8.2 && x < -5.8) && ( y < -0.8 && y > -7.2 )){
-            return false;
+            return -1;
         }
 
 
         if (( x > -13.2 && x < -3.8) && ( y < 5.2 && y >0.8 )){
-            return false;
+            return -1;
         }
-
-
-
-        //Colisões dos blocos acabada
-
-
-
-
-
-
 
         //Paredes Interiores
 
-
-
-
         if (( x > -17.2 && x < 0.2) && ( y < 9.2  &&  y > 8.8)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -0.2 && x <0.2) && ( y < 9.2  &&  y > 2.8)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -0.2 && x < 13.2) && ( y < 3.2  &&  y > 2.8)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > 12.8 && x < 13.2) && ( y < 3.2  &&  y > 0.8)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > 12.8 && x < 16.2) && ( y < 1.2  &&  y > 0.8)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > 8.8 && x < 16.2) && ( y < 5.2  &&  y > 4.8)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > 15.8 && x < 16.2) && ( y < 18.2  &&  y > -14.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > 17.8 && x < 18.2) && ( y < 18.2  &&  y > -14.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > 15.8 && x < 18.2) && ( y <  -13.8 &&  y > -14.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > 13.8 && x < 14.2) && ( y < -15.8  &&  y > -18.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -0.2 && x < 19.8) && ( y < -17.8  &&  y > -18.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -4.2 && x < -3.8) && ( y < 9.2  &&  y > -13.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -6.2 && x < -3.8) && ( y < -12.8  &&  y > -13.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -6.2 && x < -5.8) && ( y < -6.8 &&  y > -13.2)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > -10.2 && x < -5.8) && ( y < -6.8  &&  y > -7.2)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > -15.2 && x < -11.8) && ( y < -6.8  &&  y > -7.2)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > -15.2 && x < -14.8) && ( y < 7.2 &&  y > -7.2)){
-            return false;
+            return -1;
         }
-
 
         if (( x > 15.4 && x < -14.8) && ( y < 7.2 &&  y > -7.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -15.2 && x < -3.8) && ( y < 7.2  &&  y > 6.8)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > -17.2 && x < -16.8) && ( y < 9.2  &&  y > -19.8)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -17.2 && x < -14.8) && ( y < -17.8  &&  y > -18.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -15.2 && x < -14.8) && ( y < -9.8  &&  y > -18.2)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > -15.2 && x < -8.8) && ( y < -9.8  &&  y > -10.2)){
-            return false;
+            return -1;
         }
-
-
 
         if (( x > -12.2 && x < -11.8) && ( y < -9.8  &&  y > -18.2)){
-            return false;
+            return -1;
         }
-
-
-
 
         if (( x > -9.2 && x < -8.8) && ( y < -9.8  &&  y > -18.2)){
-            return false;
+            return -1;
         }
 
-
-
-
-         if (( x > 15.8 && x < 16.2) && ( y < -15.8  &&  y > -18.2)){
-            return false;
+        if (( x > 15.8 && x < 16.2) && ( y < -15.8  &&  y > -18.2)){
+            return -1;
         }
 
+        // buracos
+        if((pow((x + 0.791),2.0) + (pow((y - 0.1471),2.0))) <= (pow(1,2))){
+            return -2;
+        }
 
+        if((pow((x - 5.627),2.0) + (pow((y + 0.9405),2.0))) <= (pow(1.4,2))){
+            return -2;
+        }
 
+        if((pow((x + 4.519),2.0) + (pow((y + 13.92),2.0))) <= (pow(1,2))){
+            return -2;
+        }
 
+        if((pow((x + 5.0),2.0) + (pow((y),2.0))) <= (pow(1.4,2))){
+            return -2;
+        }
 
+        if((pow((x - 5.312),2.0) + (pow((y - 8.12),2.0))) <= (pow(1.4,2))){
+            return 0;
+        }
 
+        if((pow((x + 16.81),2.0) + (pow((y + 11.89),2.0))) <= (pow(1,2))){
+            return -2;
+        }
 
+        if((pow((x + 18.77),2.0) + (pow((y + 13.45),2.0))) <= (pow(1.4,2))){
+            return -2;
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if((pow((x + 17.96),2.0) + (pow((y + 15.16),2.0))) <= (pow(1.2,2))){
+            return -2;
+        }
 
         return true;
     }
@@ -375,23 +287,43 @@ public:
         float velocity = MovementSpeed * deltaTime;
         if (direction == FORWARD){
             Position += Front * velocity;
-            if(!EvaluateCoordinates(Position.x, Position.z))
+            if(EvaluateCoordinates(Position.x, Position.z) == -1)
                 Position = aux_Position;
+            else if(EvaluateCoordinates(Position.x, Position.z) == -2){
+                Position.x = -13.5;
+                Position.y = 0.5f;
+                Position.z = 16.0f;
+            }
         }
         if (direction == BACKWARD){
             Position -= Front * velocity;
-            if(!EvaluateCoordinates(Position.x, Position.z))
+            if(EvaluateCoordinates(Position.x, Position.z) == -1)
                 Position = aux_Position;
+            else if(EvaluateCoordinates(Position.x, Position.z) == -2){
+                Position.x = -13.5;
+                Position.y = 0.5f;
+                Position.z = 16.0f;
+            }
         }
         if (direction == LEFT){
             Position -= Right * velocity;
-            if(!EvaluateCoordinates(Position.x, Position.z))
+            if(EvaluateCoordinates(Position.x, Position.z) == -1)
                 Position = aux_Position;
+            else if(EvaluateCoordinates(Position.x, Position.z) == -2){
+                Position.x = -13.5;
+                Position.y = 0.5f;
+                Position.z = 16.0f;
+            }
         }
         if (direction == RIGHT){
             Position += Right * velocity;
-            if(!EvaluateCoordinates(Position.x, Position.z))
+            if(EvaluateCoordinates(Position.x, Position.z) == -1)
                 Position = aux_Position;
+            else if(EvaluateCoordinates(Position.x, Position.z) == -2){
+                Position.x = -13.5;
+                Position.y = 0.5f;
+                Position.z = 16.0f;
+            }
         }
     }
 
